9bb27031aa92aa647b204cf17581e6a8d11781b3
susu <28025920@qq.com>
2016-04-25 12:59:40 +0800

    添加图片动画时的阴影效果

--- Folding effect/Folding effect/ViewController.m
+++ Folding effect/Folding effect/ViewController.m
@@ -13,9 +13,25 @@
 
 @property (strong, nonatomic) IBOutlet UIImageView *topImageView;
 @property (strong, nonatomic) IBOutlet UIImageView *bottomImageView;
+
+@property(strong,nonatomic)CAGradientLayer* gradientLayer;
 @end
 
 @implementation ViewController
+
+//懒加载阴影图层
+-(CAGradientLayer *)gradientLayer{
+    
+    if (_gradientLayer==nil) {
+        _gradientLayer=[CAGradientLayer layer];
+        _gradientLayer.frame=self.bottomImageView.bounds;
+        _gradientLayer.colors=@[(id)[UIColor clearColor].CGColor,(id)[UIColor blackColor].CGColor];
+        _gradientLayer.opacity=0.0;
+        [self.bottomImageView.layer addSublayer:_gradientLayer];
+        
+    }
+    return _gradientLayer;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -29,7 +45,6 @@
     UIPanGestureRecognizer* pan=[[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(pan:)];
     [self.dragView addGestureRecognizer:pan];
     
-    
 }
 
 -(void)pan:(UIPanGestureRecognizer*)pan{
@@ -38,13 +53,23 @@
     
     CGFloat angle=-transP.y/self.dragView.bounds.size.height*M_PI;
     
+    CGFloat opacity=transP.y/self.dragView.bounds.size.height;
+    
     self.topImageView.layer.transform=CATransform3DMakeRotation(angle, 1, 0, 0);
     
+    self.gradientLayer.opacity=opacity;
+    
     if (pan.state==UIGestureRecognizerStateEnded) {
-        [UIView animateWithDuration:0.5 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-            self.topImageView.layer.transform=CATransform3DIdentity;
-        } completion:^(BOOL finished) {
+        
+        //弹簧效果动画
+        [UIView animateWithDuration:0.5 delay:0.0 usingSpringWithDamping:0.2 initialSpringVelocity:0.5 options:UIViewAnimationOptionCurveEaseInOut animations:^{
             
+            self.gradientLayer.opacity=0.0;
+            
+            self.topImageView.layer.transform=CATransform3DIdentity;
+            
+        } completion:^(BOOL finished) {
+        
         }];
     }
 }
